8e9cb55bbd6f2d854378dde95cb0e336c5379678
Roy <roy.rojval@gmail.com>
2021-07-29 18:36:29 -0500

    Sesion03

---
+++ sesion03.sql
@@ -0,0 +1,17 @@
+/*
+Ejemplo 3 : 
+Crear nuevamente la base de datos “Sesion03” utilizando comandos de creación
+Crear la tabla usuarios con las siguientes columnas
+ID				|		entero
+DNI				|		cadena(8)
+FechaNacimiento |		fecha
+sexo			|		caracter (1)
+Nombre			|		cadena	(30)
+ApellidoPaterno	|		cadena (80)
+ApellidoMaterno	|		cadena (80)
+
+
+Agregar una nueva columna Email de tipo cadena (50)
+modificar la columna Email a tipo cadena  (80)
+eliminar la columna Email
+*/
