e9510457756ae9095c970d9305e4431fe01eb267
Roy <roy.rojval@gmail.com>
2021-07-29 19:34:39 -0500

    Sesion 03

--- sesion03.sql
+++ sesion03.sql
@@ -15,3 +15,49 @@ Agregar una nueva columna Email de tipo cadena (50)
 modificar la columna Email a tipo cadena  (80)
 eliminar la columna Email
 */
+-- Creando una nueva tabla
+create table usuarios (
+	ID				int,
+	DNI				char(8),
+	FechaNacimiento	datetime,
+	Sexo			char(1),
+	Nombre			varchar(30),
+	ApellidoPaterno	varchar(80),
+	ApellidoMaterno	varchar(80)
+)
+go
+-- Seleccionando los registros de la tabla
+Select * from usuarios
+Go
+
+sp_help 'usuarios'
+go
+
+--Agregando una nueva columna
+ALTER TABLE usuarios ADD Email  varchar (50)
+--verificando que la nueva columna fue agregada
+Select * from usuarios
+go
+
+-- Editando la nueva columna agregada
+Alter table usuarios alter column Email varchar (80)
+Select * from usuarios
+GO 
+-- Verificando que la nueva columna fue modificada
+sp_help 'usuarios'
+go
+
+
+--Eliminando la nueva columna
+alter table usuarios drop column Email
+Select * from usuarios
+go
+
+-- Verificando que la nueva columna fue modificada
+sp_help 'usuarios'
+go
+
+/*
+En caso se quiera eliminar la table
+DROP table usuarios
+*/
