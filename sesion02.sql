6b38ba05f3c3771a81d0d6060533e2e8a7682d55
Roy <roy.rojval@gmail.com>
2021-08-05 22:30:42 -0500

    update

--- sesion02.sql
+++ sesion02.sql
@@ -1,5 +1,5 @@
 /*
-Ejemplo 3 : 
+Ejemplo 2 : 
 Crear nuevamente la base de datos “Sesion02” utilizando comandos de creación
 Especificar los archivos primarios , secundarios y de registro, indicando el tamaño, el tamaño máximo y su factor de crecimiento
 Finalmente borrar la base de datos con comandos de borrado
