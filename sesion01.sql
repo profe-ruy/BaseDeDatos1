962d2f885a46fc2895c6993aa100bf2d24494001
Roy <roy.rojval@gmail.com>
2021-08-05 22:32:30 -0500

    update sesion 01 file

--- sesion01.sql
+++ sesion01.sql
@@ -2,7 +2,7 @@
 Ejemplo 01
 Crear una base de datos “MiPrimeraBD” 
 Crear una nueva tabla “Alumnos”  que tenga las siguientes propiedades
-IdAlumno
+“IdAlumno” 
 Dni
 Nombre 
 ApellidoPaterno 
@@ -13,4 +13,24 @@ FechaNacimiento
 FechaIngreso
 
 */
- 
+
+--Crear la BD
+create database MiPrimeraDB
+go
+--Usar la bd
+use MiPrimeraDB 
+--Crear la tabla
+create table tb_Alumnos (
+IdAlumno		int ,
+Dni				varchar(50),
+Nombre			varchar(50),
+ApellidoPaterno varchar(50),
+ApellidoMaterno varchar(50),
+Edad			int, 
+Sexo			char(1),
+FechaNacimiento datetime,
+FechaIngreso	datetime
+)
+go
+--Seleccionar la tabla
+select * from tb_Alumnos
