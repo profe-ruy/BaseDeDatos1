1af51c4dce8c0117ca21a547b7722e7fda8400aa
Roy <roy.rojval@gmail.com>
2021-08-15 21:40:23 -0500

    agregando ejercicio 5

---
+++ sesion05.sql
@@ -0,0 +1,18 @@
+/*
+Ejemplo 5 : 
+Crear nuevamente la base de datos Sesion05 utilizando comandos de creacion
+Crear la tabla empresa con las siguientes columnas
+ID				|		int 
+RUC				|		cadena(8)
+FechaCreacion   |		fecha 
+RazonSocial		|		cadena	(50)
+Activa          |       boolean 
+Ciudad			|		cadena (30)
+Estado			|		cadena (20)
+
+Generar la columna ID como primary key,
+Considerar que el ruc de una empresa tiene que ser unica y no repetirse 
+Considerar que la razon social no puede ser nula
+Considerar que si no se ingresa la fecha de creacion de la empresa debera tomar el valor del sistema
+Considerar que el estado de la empresa solo pueden ser 'Recien Creada', 'En ejercicio' , 'Cerrada' o 'En proceso de cierre'
+*/
